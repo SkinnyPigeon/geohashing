70c31788aad53469953e1f15dc6c52aadf3e1cc7
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-19 22:29:52 +0100

    can import compressed file

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import pygeohash as pgh
+import gzip
+
+from pyparsing import col
 
 
 class UniqueGeoHash:
     def is_file_compressed(self, filename):
         extension = filename.split('.')[-1]
-        if extension in ['gz', 'zip']:
+        if extension in ['gz']:
             return extension
         return False
+
+    def import_compressed_file(self, filename, extension):
+        if extension == 'gz':
+            with gzip.open(filename, 'rt') as f:
+                return self.format_file(f)
+
+    def format_file(self, open_file):
+        coords = {'lat': [], 'lng': []}
+        for i, line in enumerate(open_file):
+            columns = line.rstrip('\n').split(',')
+            if i == 0:
+                lat_index = columns.index('lat')
+                lng_index = columns.index('lng')
+            else:
+                coords['lat'].append(columns[lat_index])
+                coords['lng'].append(columns[lng_index])
+        return coords
--- tests/unique_geo_test.py
+++ tests/unique_geo_test.py
@@ -5,3 +5,16 @@ def test_knows_if_file_is_compressed():
     ugh = UniqueGeoHash()
     assert ugh.is_file_compressed('test_points.txt.gz') == 'gz'
     assert ugh.is_file_compressed('test_points.txt') is False
+
+
+def test_can_import_compressed_file():
+    ugh = UniqueGeoHash()
+    imported_file = ugh.import_compressed_file('test_points_test.txt.gz', 'gz')
+    assert imported_file['lat'] == ['41.37484808',
+                                    '41.37438124',
+                                    '41.3806658',
+                                    '41.389865']
+    assert imported_file['lng'] == ['2.11886538',
+                                    '2.11842003',
+                                    '2.1647467',
+                                    '2.1556433']
