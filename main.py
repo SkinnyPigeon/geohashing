82f073ac0467d9f828a1a73dc66575bb4ab432fa
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-19 22:53:05 +0100

    added the geohash during the import

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygeohash as pgh
 import gzip
 
-from pyparsing import col
-
 
 class UniqueGeoHash:
     def is_file_compressed(self, filename):
@@ -21,13 +19,23 @@ class UniqueGeoHash:
             return self.format_file(f)
 
     def format_file(self, open_file):
-        coords = {'lat': [], 'lng': []}
+        coords = {'lat': [], 'lng': [], 'geohash': []}
         for i, line in enumerate(open_file):
             columns = line.rstrip('\n').split(',')
             if i == 0:
                 lat_index = columns.index('lat')
                 lng_index = columns.index('lng')
             else:
-                coords['lat'].append(columns[lat_index])
-                coords['lng'].append(columns[lng_index])
+                lat_str = columns[lat_index]
+                lng_str = columns[lng_index]
+                precision = len(lat_str.split('.')[1] + lng_str.split('.')[1])
+                lat_float = float(lat_str)
+                lng_float = float(lng_str)
+                coords['lat'].append(lat_float)
+                coords['lng'].append(lng_float)
+                coords['geohash'].append(pgh.encode(
+                    lat_float,
+                    lng_float,
+                    precision=precision
+                ))
         return coords
--- tests/unique_geo_test.py
+++ tests/unique_geo_test.py
@@ -13,14 +13,18 @@ def test_can_import_compressed_file():
         'data/test_points_test.txt.gz',
         'gz'
     )
-    assert imported_file['lat'] == ['41.37484808',
-                                    '41.37438124',
-                                    '41.3806658',
-                                    '41.389865']
-    assert imported_file['lng'] == ['2.11886538',
-                                    '2.11842003',
-                                    '2.1647467',
-                                    '2.1556433']
+    assert imported_file['lat'] == [41.37484808,
+                                    41.37438124,
+                                    41.3806658,
+                                    41.389865]
+    assert imported_file['lng'] == [2.11886538,
+                                    2.11842003,
+                                    2.1647467,
+                                    2.1556433]
+    assert imported_file['geohash'] == ['sp3e2hncvnsw3r1h',
+                                        'sp3e25yx4z8mwv4r',
+                                        'sp3e3m07f70ns0',
+                                        'sp3e3nc9qnh1u']
 
 
 def test_can_import_non_compressed_file():
@@ -28,11 +32,15 @@ def test_can_import_non_compressed_file():
     imported_file = ugh.import_non_compressed_file(
         'data/test_points_test.txt'
     )
-    assert imported_file['lat'] == ['41.37484808',
-                                    '41.37438124',
-                                    '41.3806658',
-                                    '41.389865']
-    assert imported_file['lng'] == ['2.11886538',
-                                    '2.11842003',
-                                    '2.1647467',
-                                    '2.1556433']
+    assert imported_file['lat'] == [41.37484808,
+                                    41.37438124,
+                                    41.3806658,
+                                    41.389865]
+    assert imported_file['lng'] == [2.11886538,
+                                    2.11842003,
+                                    2.1647467,
+                                    2.1556433]
+    assert imported_file['geohash'] == ['sp3e2hncvnsw3r1h',
+                                        'sp3e25yx4z8mwv4r',
+                                        'sp3e3m07f70ns0',
+                                        'sp3e3nc9qnh1u']
